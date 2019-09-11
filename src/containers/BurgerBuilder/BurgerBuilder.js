444795a009313ea773b6054463fadf8fcf15203c
halfed <1half.ed@gmail.com>
2019-09-11 17:35:39 -0600

    adding initial files

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxilary';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+	salad: 0.5, 
+	cheese: 0.4,
+	meat: 1.3,
+	bacon: 0.7
+}
+
+class BurgerBuilder extends Component {
+	// constuctor(props) {
+	// 	super(props);
+	// 	this.state = {...}
+	// }
+
+	state = {
+		ingredients: {
+			salad: 0,
+			bacon: 0,
+			cheese: 0,
+			meat: 0
+		},
+		totoalPrice: 4
+	}
+
+	addIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		const updatedCounted = oldCount + 1;
+		const updateIngredients = {
+			...this.state.ingredients
+		};
+		updateIngredients[type] = updatedCounted;
+		const priceAddition = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totoalPrice;
+		const newPrice = oldPrice + priceAddition;
+		this.setState({totoalPrice: newPrice, ingredients:updateIngredients});
+	}
+
+	removeIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		if(oldCount <= 0) {
+			return;
+		}
+		const updatedCounted = oldCount - 1;
+		const updateIngredients = {
+			...this.state.ingredients
+		};
+		updateIngredients[type] = updatedCounted;
+		const priceDeduction = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totoalPrice;
+		const newPrice = oldPrice - priceDeduction;
+		this.setState({totoalPrice: newPrice, ingredients:updateIngredients});
+	}
+
+
+	render () {
+		const disabledInfo = {
+			...this.state.ingredients
+		}
+		for(let key in disabledInfo) {
+			disabledInfo[key] = disabledInfo[key] <= 0
+		}
+		return (
+			<Aux>
+				<Burger ingredients={this.state.ingredients} />
+				<BuildControls
+					ingredientAdded ={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+					disabled={disabledInfo}
+				/>
+			</Aux>
+		);
+	}
+}
+
+export default BurgerBuilder;
