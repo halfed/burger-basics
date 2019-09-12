0a88f4702eeab0fd822d0ff79fdf34d62327248d
halfed <1half.ed@gmail.com>
2019-09-12 17:26:32 -0600

    adding and styling total price

--- src/components/Burger/OderSummary/OderSummary.js
+++ src/components/Burger/OderSummary/OderSummary.js
@@ -17,6 +17,7 @@ const orderSummary = (props) => {
 			<ul>
 				{ingredientsSummary}
 			</ul>
+			<p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
 			<p>Continue to Checkout?</p>
 			<Button btnType="Danger" clicked={props.purchasedCancelled}>CANCEL</Button>
 			<Button btnType="Success" clicked={props.purchasedContinued}>CONTINUE</Button>
