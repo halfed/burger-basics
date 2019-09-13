888538152dee731b8e0a5bbed0d4c40eebf1e76a
halfed <1half.ed@gmail.com>
2019-09-12 22:52:31 -0600

    adding closing functionality to side drawer

--- src/components/Burger/OderSummary/OderSummary.js
+++ src/components/Burger/OderSummary/OderSummary.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Auxilary';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-	const ingredientsSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+	///this could be a functional component doesn't have to be a class
+	componentWillUpdate() {
+		console.log('["OrderedSummary"] will update');
+	}
+
+	render() {
+		const ingredientsSummary = Object.keys(this.props.ingredients)
 		.map(igKey => {
 			return (<li key={igKey}>
 						<span style={{ textTransform: 'capitalize' }}>{igKey}</span>: 
-						{props.ingredients[igKey]}
+						{this.props.ingredients[igKey]}
 					</li>);
 		});
-	return (
-		<Aux>
-			<h3>Your Order</h3>
-			<p>A delicious burger with the following ingredients:</p>
-			<ul>
-				{ingredientsSummary}
-			</ul>
-			<p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-			<p>Continue to Checkout?</p>
-			<Button btnType="Danger" clicked={props.purchasedCancelled}>CANCEL</Button>
-			<Button btnType="Success" clicked={props.purchasedContinued}>CONTINUE</Button>
-		</Aux>
-	)
+		return(
+			<Aux>
+				<h3>Your Order</h3>
+				<p>A delicious burger with the following ingredients:</p>
+				<ul>
+					{ingredientsSummary}
+				</ul>
+				<p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+				<p>Continue to Checkout?</p>
+				<Button btnType="Danger" clicked={this.props.purchasedCancelled}>CANCEL</Button>
+				<Button btnType="Success" clicked={this.props.purchasedContinued}>CONTINUE</Button>
+			</Aux>
+	)}
 }
 
-export default orderSummary;
+export default OrderSummary;
