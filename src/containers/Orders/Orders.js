f99ab1cbf0c8c9fec19967c939289a84be179348
halfed <1half.ed@gmail.com>
2020-05-24 22:44:34 -0600

    removing a console.log

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -19,7 +19,7 @@ class Orders extends Component {
                         id: key
                     })
                 }
-                console.log(res.data);
+                //console.log(res.data);
                 this.setState({ loading: false, orders: fetchedOrders });
             })
             .catch(err => {
